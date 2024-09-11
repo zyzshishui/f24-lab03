de7dfd0b4277f7d6d833f8ad7c685cf488b1f599
zyzshishui <492129152@qq.com>
2024-09-11 17:17:45 -0400

    feat: Java Delegation

--- Java/src/main/java/edu/cmu/cs/cs214/rec04/DelegationSortedIntList.java
+++ Java/src/main/java/edu/cmu/cs/cs214/rec04/DelegationSortedIntList.java
@@ -7,10 +7,88 @@ package edu.cmu.cs.cs214.rec04;
  * and exports an accessor (totalAdded) for this count.
  *
  * @author Nora Shoemaker
- *
  */
 
-public class DelegationSortedIntList {
-    // Write your implementation below with API documentation
+public class DelegationSortedIntList implements IntegerList {
+    private int totalAdded;
+    private SortedIntList sortedIntList;
 
+    public DelegationSortedIntList() {
+        this.sortedIntList = new SortedIntList();
+    }
+
+    /**
+     * Adds the specified int to the list.
+     *
+     * @param num an integer to be added to the list
+     * @return true if the list is changed as a result of the call
+     */
+    @Override
+    public boolean add(int num) {
+        this.totalAdded++;
+        return sortedIntList.add(num);
+    }
+
+    /**
+     * Adds all of the elements of the IntegerList to the list.
+     *
+     * @param list IntegerList containing elements to be added to the list
+     * @return true if the list changed as a result of the call
+     */
+    @Override
+    public boolean addAll(IntegerList list) {
+        this.totalAdded += list.size();
+        return sortedIntList.addAll(list);
+    }
+
+    /**
+     * Returns the integer at the specified position in this list.
+     *
+     * @param index index of the element to return
+     * @return the element at the specified position in this list
+     */
+    @Override
+    public int get(int index) {
+        return sortedIntList.get(index);
+    }
+
+    /**
+     * Removes the first occurrence of the specified element from the list,
+     * if it is present (optional operation).
+     *
+     * @param num an integer to be removed from the list, if present
+     * @return true if an element was removed as a result of this call
+     */
+    @Override
+    public boolean remove(int num) {
+        return sortedIntList.remove(num);
+    }
+
+    /**
+     * Removes from the list all of its elements that are contained in the
+     * specified IntegerList.
+     *
+     * @param list IntegerList containing elements to be removed from
+     *             the list
+     * @return true if the list changed as a result of the call
+     */
+    @Override
+    public boolean removeAll(IntegerList list) {
+        return sortedIntList.removeAll(list);
+    }
+
+    /**
+     * Returns the number of elements in this list. If this list contains
+     * more than Integer.MAX_VALUE elements, returns Integer.MAX_VALUE.
+     *
+     * @return number of elements in the list
+     */
+    @Override
+    public int size() {
+        return sortedIntList.size();
+    }
+
+    public int getTotalAdded() {
+        return this.totalAdded;
+    }
 }
